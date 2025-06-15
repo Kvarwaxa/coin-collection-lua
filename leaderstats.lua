8e8fc1e914bb264d0b7befbe294995997ad1c83e
Kvarwaxa <andriapubg1@gmail.com>
2025-06-16 00:44:27 +0400

    Create leaderstats.lua

---
+++ leaderstats.lua
@@ -0,0 +1,10 @@
+game.Players.PlayerAdded:Connect(function(player)
+	local stats = Instance.new("Folder")
+	stats.Name = "leaderstats"
+	stats.Parent = player
+
+	local coins = Instance.new("IntValue")
+	coins.Name = "Coins"
+	coins.Value = 0
+	coins.Parent = stats
+end)
